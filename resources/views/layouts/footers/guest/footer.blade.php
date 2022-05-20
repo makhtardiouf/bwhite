e0d4ab92bc40481f31d614bc87c3c172107ac7b8
Makhtar Diouf <makhtar.diouf@gmail.com>
2022-05-20 12:02:06 +0000

    ++

---
+++ resources/views/layouts/footers/guest/footer.blade.php
@@ -0,0 +1,40 @@
+  <!-- -------- START FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
+  <footer class="footer py-5">
+    <div class="container">
+      <div class="row">
+      <div class="col-lg-8 mb-4 mx-auto text-center">
+          <a href="#" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+              Bwhite
+          </a>
+          <a href="#" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+              Apropos
+          </a>
+          
+      </div>
+        @if (!auth()->user() || \Request::is('static-sign-up')) 
+          <div class="col-lg-8 mx-auto text-center mb-4 mt-2">
+              <a href="#" target="_blank" class="text-secondary me-xl-4 me-4">
+                  <span class="text-lg fab fa-dribbble" aria-hidden="true"></span>
+              </a>
+              <a href="#" target="_blank" class="text-secondary me-xl-4 me-4">
+                  <span class="text-lg fab fa-twitter" aria-hidden="true"></span>
+              </a>
+              <a href="#" target="_blank" class="text-secondary me-xl-4 me-4">
+                  <span class="text-lg fab fa-instagram" aria-hidden="true"></span>
+              </a>
+          </div>
+        @endif
+      </div>
+      @if (!auth()->user() || \Request::is('static-sign-up')) 
+        <div class="row">
+          <div class="col-8 mx-auto text-center mt-1">
+            <p class="mb-0 text-secondary">
+              Copyright © <script>
+                document.write(new Date().getFullYear())
+              </script> Bwhite Sénégal
+            </p>
+          </div>
+        </div>
+      @endif
+    </div>
+  </footer>
