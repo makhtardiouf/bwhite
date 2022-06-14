b02e7c0dc8d1f97bdf8d0681c029d127c990d6d5
Makhtar Diouf <makhtar.diouf@gmail.com>
2022-06-14 11:51:20 +0000

    Layout login and register

--- resources/views/session/login-session.blade.php
+++ resources/views/session/login-session.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
 
-  <main class="main-content  mt-0">
+  <main class="main-content">
     <section>
-      <div class="page-header min-vh-75">
+      <div class="page-header">
         <div class="container">
           <div class="row">
             <div class="col-xl-4 col-lg-5 col-md-6 d-flex flex-column mx-auto">
-              <div class="card card-plain mt-8">
+              <div class="card card-plain">
                 <div class="card-header pb-0 text-left bg-transparent">
                   <h3 class="font-weight-bolder">Bienvenue</h3>
                 </div>
