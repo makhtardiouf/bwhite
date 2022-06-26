04dce04ecd38879d9e3e1d84272fa3f6f16b2eaf
Makhtar Diouf <makhtar.diouf@gmail.com>
2022-06-26 13:19:59 +0000

    Listing details

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -3,38 +3,51 @@
 Contenu de l'annonce
 @endsection
 @section('content')
-<section class="section">
-    <div class="section-header">
-        <h4 class="text-info text-gradient-dark">Contenu de l'annonce</h4>
-    </div>
-    @include('adminlte-templates::common.errors')
-    <div class="section-body">
-        <div class="card">
-            <div class="card-body col-md-12 col-lg-8">
-                @include('listings.show_fields')
+<main class="main-content  mt-0">
+    <div class="col-12 mt-4">
+        <div class="card mb-4">
+            <div class="card-header pb-0 p-3">
 
-                <div class="section-header-breadcrumb">
-                    <a href="{{ route('listings.index') }}" class="btn btn-sm bg-gradient-dark float-right">Retour</a>
+            </div>
+            <div class="card-body p-3">
+                <div class="row">
+                    <div class="col-xl-6 col-md-12 mb-4">
+                        <div class="card card-plain">
+                            <div class="position-relative">
+                                <a class="shadow-xl border-radius-xl">
+                                    <img src="{{ asset('storage/'.$listing->image) }}" alt="img-blur-shadow" class="img-fluid shadow border-radius-xl">
+                                </a>
+                            </div>
+                            <div class="card-body px-1 pb-0">
+                                <a href="javascript:;">
+                                    <span class="h4">
+                                        <!-- {{ $listing->category }}:-->
+                                        {{ $listing->title }} 
+                                    </span>
+                                </a>
+                                <p class="mb-4">
+                                <div>{!! $listing->description !!}</div>
+                                </p>
+
+                                <div class="d-flex align-items-center justify-content-between">
+                                    <div class="form-group col-sm-6 float-right">
+                                        <a href="{{ route('listings.index') }}" class="btn btn-outline-dark btn-sm m-2">Retour</a>
+                                        <a href="/listings/{{$listing->id}}/edit" class="btn btn-outline-primary btn-sm mb-0">Editer</a>
+                                    </div>
+                                    <div class="avatar-group mt-2">
+                                        <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Elena Morison">
+                                            <img alt="Image placeholder" src="../assets/img/team-1.jpg">
+                                        </a>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
     </div>
-</section>
-@endsection
-
-@section('extrascripts')
-@section('extrascripts')
-<script src="https://cdn.ckeditor.com/ckeditor5/34.1.0/classic/ckeditor.js"></script>
-<script>
-    var editor = ClassicEditor
-        .create(document.querySelector('#description'))
-        .then(editor => {
-            editor.enableReadOnlyMode('description');
-            editor.isReadOnly;
-            console.log(editor);
-        })
-        .catch(error => {
-            console.error(error);
-        });
-</script>
+</main>
 @endsection
