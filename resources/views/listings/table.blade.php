cec0a9cf3bc6fdddc19df0e7ab14ed151eee3bd2
Makhtar Diouf <makhtar.diouf@gmail.com>
2022-06-26 17:01:40 +0000

    Pre-template switch

--- resources/views/listings/table.blade.php
+++ resources/views/listings/table.blade.php
@@ -1,25 +1,26 @@
 <div class="table-responsive">
-    <table class="table" id="listings-table">
+    <table class="table table-striped table-sm" id="listings-table">
         <thead>
             <tr>
                 <th>Titre</th>
                 <th>Catégorie</th>
                 <th>Prix (XOF)</th>
-                <th colspan="3">Action</th>
+                <th colspan="2" class="d-none"></th>
             </tr>
         </thead>
         <tbody>
             @foreach($listings as $listing)
             <tr>
-                <td>{{ $listing->title }}</td>
+                <td><a href="{!! route('listings.show', [$listing->id]) !!}" >
+                       {{ $listing->title }} </a>
+                </td>
                 <td>{{ $listing->category }}</td>
                 <td><span class="text-truncate">{{ $listing->price }}</span></td>
 
-                <td class=" text-center">
+                <td class=" text-center" class="d-none">
                     {!! Form::open(['route' => ['listings.destroy', $listing->id], 'method' => 'delete']) !!}
                     <div class='btn-group'>
-                        <a href="{!! route('listings.show', [$listing->id]) !!}" class='btn btn-light action-btn '><i class="fa fa-eye"></i></a>
-                        <a href="{!! route('listings.edit', [$listing->id]) !!}" class='btn btn-warning action-btn edit-btn'><i class="fa fa-edit"></i></a>
+                         <a href="{!! route('listings.edit', [$listing->id]) !!}" class='btn btn-warning action-btn edit-btn'><i class="fa fa-edit"></i></a>
                         {!! Form::button('<i class="fa fa-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger action-btn delete-btn', 'onclick' => 'return confirm("Confirmez-vous la suppression ?")']) !!}
                     </div>
                     {!! Form::close() !!}
